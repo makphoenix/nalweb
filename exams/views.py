746c3284e062187c0d463bb6e1a0bfa47d4d525b
Mohammad Ali Kashi <mohammad.ali.kashi@gmail.com>
2020-05-23 02:20:33 +0430

    1- Exams: Part 3

--- exams/views.py
+++ exams/views.py
@@ -3,4 +3,16 @@ from django.contrib.auth.decorators import login_required
 
 
 def exam01(request):
-    return render(request, 'exams/exam01_cast.html')
+    if request.method == 'POST':
+        exam01_mark = 0
+
+        # Question 1
+        input1_1 = float(request.POST['input1_1'])
+        input1_2 = float(request.POST['input1_2'])
+        if abs(input1_1 - 1.194) <= 0.001:
+            exam01_mark += 1
+
+        if abs(input1_2 - 0.0012965) <= 0.0000001:
+            exam01_mark += 1
+    else:
+        return render(request, 'exams/exam01_cast.html')
